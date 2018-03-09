1496171da3490c3141cacf493e9c860299a0d648
parker lindley <fugazi8@gmail.com>
2018-03-08 20:51:53 -0700

    Add main component, set up structure

---
+++ src/components/IdeaList.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <ul>
+      <li>idea one</li>
+    </ul>
+  </section>
+</template>
+
+<script type = "text/javascript" >
+
+export default {};
+
+</script>
+
+<style>
+  section {
+    background: rgba( 20, 20, 155, 0.4);
+    color: white;
+    height: 250px;
+    width:350px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    text-align: center;
+  }
+
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import IdeaList from '@/components/IdeaList'
+Vue.use(Router)
+
+export default new Router({
+  IdeaList,
+  routes: [
+    {
+      path: '/',
+      name: 'IdeaList',
+      component: IdeaList
+    }
+  ]
+})
